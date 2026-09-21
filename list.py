7d65af4a6e97fb0d1f79acc3d67adeff3c2c232b
250851pau <250851@utxicotepec.edu.mx>
2026-09-21 08:59:06 -0600

    se realizo el metodo burbuja

---
+++ list.py
@@ -0,0 +1,11 @@
+#Recorrido de lista 1
+lista = [10,20,30,40,50,60]
+print(lista[::])
+#for x in lista:
+    #print(x)
+
+#Recorrido de lista 2
+#for i in range(len(lista)):
+    #print(lista[i])
+
+
